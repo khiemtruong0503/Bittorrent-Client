d355a9e0700e71773b5b2572a007ca19b1b1a6d3
unknown <kiritom115@gmail.com>
2024-04-29 19:37:04 +0700

    pass stage

--- src/Main.cpp
+++ src/Main.cpp
@@ -6,10 +6,12 @@
 #include <fstream> 
 #include <sstream>
 #include <utility>
+#include <random>
+#include <tuple>
 
 #include "lib/sha1.hpp"
 #include "lib/nlohmann/json.hpp"
-
+#include "lib/HTTPRequest.hpp"
 
 using json = nlohmann::json;
 
@@ -250,7 +252,140 @@ std::string json_to_bencode(const json &j) {
     return os.str();
 }
 
+std::vector<unsigned char> HexToBytes(const std::string &hex) { 
+    std::vector<unsigned char> bytes;
 
+    for (unsigned int i = 0; i < hex.length(); i += 2) { 
+        std::string byteString = hex.substr(i, 2);
+        char byte = (char)strtol(byteString.c_str(), NULL, 16);
+        bytes.push_back(byte);
+    }
+
+    return bytes;
+}
+
+std::string url_encode(const std::string &p) { 
+    std::vector<unsigned char> s(HexToBytes(p));
+    static const char lookup[] = "0123456789abcdef";
+    std::stringstream e;
+    for(int i = 0, ix = s.size(); i < ix; ++i) { 
+        const char &c = s[i];
+        if ((48 <= c && c <= 57) || // 0-9
+            (65 <= c && c <= 90) || // abc...xyz
+            (97 <= c && c <= 122) || // ABC...XYZ
+            (c == '-' || c == '_' || c == '.' || c == '~')
+        ) { 
+            e << c;
+        } else { 
+            e << '%';
+            e << lookup[(c & 0xF0) >> 4];
+            e << lookup[(c & 0x0F)];
+        }
+    }
+
+    return e.str();
+}
+
+bool isASCII(const std::string &s) { 
+    return !std::any_of(s.begin(), s.end(), [](char c) { 
+        return static_cast<unsigned char>(c) > 127;
+    });
+}
+
+namespace ns { 
+    struct Info { 
+        std::int64_t length;
+        std::string name;
+        std::int64_t piece_length;
+        std::string pieces;
+    };
+};
+
+struct Torrent { 
+    std::string info_hash;
+    std::string announce;
+    ns::Info info;
+    std::string peer_id;
+    std::uint64_t port;
+    std::uint64_t uploaded;
+    std::uint64_t downloaded;
+    std::uint64_t left;
+    bool compacct;
+};
+
+struct TrackerResponse { 
+    std::int32_t interval;
+    std::string peers;
+};
+
+std::vector<std::tuple<std::string, std::uint32_t, std::string>> get_peers(json object, bool compact = false) { 
+    if (compact) { 
+        std::vector<std::tuple<std::string, std::uint32_t, std::string>> result;
+        std::string peers = object.template get<std::string>();
+        for(int i = 0; i < peers.length(); i += 6) { 
+            std::string peer_ip = peers.substr(i, 4);
+            std::string peer_port = peers.substr(i + 4, 2);
+
+            std:uint8_t ip[4] = { 0 };
+            std::uint8_t port[2] = { 0 };
+
+            for(int j = 0; j < 4; ++j) { 
+                ip[j] = static_cast<uint8_t>(peer_ip[j]);
+            }
+
+            for(int j = 0; j < 2; ++j) { 
+                port[j] = static_cast<uint8_t>(peer_port[j]);
+            }
+
+            std::string ip_str = std::to_string(ip[0]) + "." + std::to_string(ip[1]) + "." + std::to_string(ip[2]) + "." + std::to_string(ip[3]);
+            std::uint32_t port_value = (port[0] << 8) | port[1];
+            result.emplace_back(ip_str, port_value, "");
+        }
+
+        return result;
+    }
+
+    std::vector<std::tuple<std::string, std::uint32_t, std::string>> result;
+    for(const auto &value : object) { 
+        const auto ip = value["ip"].get <std::string>();
+        const auto port = value["port"].get <std::uint32_t>();
+        const auto peer_id = to_hex_string(value["peer id"].get <std::string>());
+        result.emplace_back(ip, port, peer_id);
+    }
+
+    return result;
+}
+
+TrackerResponse discover_peers(Torrent *torrent) { 
+    char url_buffer[2048];
+    std::string url_encoded_hash = url_encode(torrent->info_hash);
+    sprintf(
+        url_buffer, 
+        "%s?info_hash=%s&peer_id=%s&port=%d&uploaded=%d&downloaded=%d&left=%d&compact=1",
+        torrent->announce.c_str(), 
+        url_encoded_hash.c_str(),
+        torrent->peer_id.c_str(),
+        torrent->port, 
+        torrent->uploaded, 
+        torrent->downloaded,
+        torrent->left);
+
+        try { 
+            http::Request request{ url_buffer };
+            const auto response = request.send("GET");
+            std::string str_response = std::string{ response.body.begin(), response.body.end()};
+            json r = decode_bencoded_value(str_response);
+            auto peers = get_peers(r["peers"], true);
+            for(const auto& [ip, port, peer_id] : peers) { 
+                std::cout << ip << ":" << port << "\n";
+            }
+        }
+        catch (const std::exception &e) { 
+            std::cerr << "Request failed, error: " << e.what() << '\n';
+        }
+
+        return { };
+}
 int main(int argc, char* argv[]) {
     if (argc < 2) {
         std::cerr << "Usage: " << argv[0] << " decode <encoded_value>" << std::endl;
